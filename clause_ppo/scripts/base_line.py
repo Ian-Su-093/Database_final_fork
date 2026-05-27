6afdbc5eefa5de6ad3656c5fd1ddf95718567241
Ian-Su-093 <051710a@gmail.com>
2026-05-27 20:31:18 +0800

    fix: move token to env

--- clause_ppo/scripts/base_line.py
+++ clause_ppo/scripts/base_line.py
@@ -1,17 +1,19 @@
 from huggingface_hub import InferenceClient
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 client = InferenceClient(
-    provider="hf-inference",
-    token="your-token-here"
+    token=os.getenv("qwen_token")
 )
 
 messages = [{
     "role": "user",
-    "content": "Please generate a SQL query according to the given question and schema."
+    "content": "Please generate a SQL query according to the given question and schema.",
 }]
 
 completion = client.chat.completions.create(
-    model="qwen/qwen2.5-coder-1.5b",
+    model="Qwen/Qwen2.5-Coder-1.5B-Instruct:featherless-ai",
     messages=messages,
     max_tokens=500,
 )
